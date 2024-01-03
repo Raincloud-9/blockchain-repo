95ba393ecb2d4d32ccd678e4c952bb1cf3c07b60
Raincloud-9 <113468574+Raincloud-9@users.noreply.github.com>
2024-01-03 19:19:43 +0000

    Update ERC-1155.sol

--- NazawebAcademy/ERC-1155.sol
+++ NazawebAcademy/ERC-1155.sol
@@ -1,2 +1,106 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Pausable.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract Web3Builders is ERC1155, Ownable, ERC1155Pausable, ERC1155Supply {
+    uint256 public publicPrice = 0.02 ether;
+    uint256 public allowListPrice = 0.01 ether;
+    uint256 public maxSupply = 1;
+    uint public maxPerWallet = 3;
+
+    bool public publicMintOpen = false;
+    bool public allowListMintOpen = true;
+
+    mapping(address => bool) allowList;
+    mapping(address => uint256) purchasesPerWallet;
+
+    constructor(address initialOwner)
+        ERC1155("ipfs://Qmaa6TuP2s9pSKczHF4rwWhTKUdygrrDs8RmYYqCjP3Hye/")
+        Ownable(initialOwner)
+    {}
+
+
+    //Create an edit function that will edit the mint windows
+    function editMintWindows(
+        bool _publicMintOpen,
+        bool _allowListMintOpen
+    ) external onlyOwner {
+        publicMintOpen = _publicMintOpen;
+        allowListMintOpen = _allowListMintOpen;
+    }
+
+    // Create a function to set the allowlist
+    function setAllowList(address[] calldata addresses) external onlyOwner {
+        for (uint256 i = 0; i< addresses.length; i++){
+            allowList[addresses[i]] = true;
+        }
+    }
+
+    function setURI(string memory newuri) public onlyOwner {
+        _setURI(newuri);
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(uint256 id, uint256 amount) internal {
+        require(purchasesPerWallet[msg.sender] + amount <= maxPerWallet, "Too many NFT's in your wallet");
+        require(id<2, "NFT does not exist");
+        require(totalSupply(id) + amount <=  maxSupply, "Sold out");
+        _mint(msg.sender, id, amount, "");
+        purchasesPerWallet[msg.sender] += amount;
+    }
+
+    function allowListMint(uint256 id, uint256 amount) public payable {
+        require(allowListMintOpen, "Allow list mint closed");
+        require(allowList[msg.sender], "Not in allow list");
+        require(msg.value == allowListPrice * amount);
+        mint(id, amount);
+    }
+
+    // add supply tracking
+    function publicMint(uint256 id, uint256 amount)
+        public
+        payable
+    {
+        require(publicMintOpen, "Public mint closed");
+        require(msg.value == publicPrice * amount, "Not enough money sent");
+        mint(id, amount);
+    }
+
+    function withdraw(address _addr) external onlyOwner {
+        uint256 balance = address(this).balance;
+        payable(_addr).transfer(balance);
+    }
+
+    function uri(uint256 _id) public view virtual override returns (string memory) {
+        require(exists(_id), "URI: non existent token");
+        return string(abi.encodePacked(super.uri(_id), Strings.toString(_id), ".json"));
+    }
+
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        public
+        onlyOwner
+    {
+        _mintBatch(to, ids, amounts, data);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address from, address to, uint256[] memory ids, uint256[] memory values)
+        internal
+        override(ERC1155, ERC1155Pausable, ERC1155Supply)
+    {
+        super._update(from, to, ids, values);
+    }
+}
