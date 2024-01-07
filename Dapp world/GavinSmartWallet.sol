fd08697715d7c3cce3acfa159ca6274e6af8f9ca
Raincloud-9 <113468574+Raincloud-9@users.noreply.github.com>
2024-01-07 17:05:09 +0000

    Update GavinSmartWallet.sol

--- Dapp world/GavinSmartWallet.sol
+++ Dapp world/GavinSmartWallet.sol
@@ -14,3 +14,36 @@ contract SmartWallet {
     constructor(){
         gavin = msg.sender;
     }
+
+    //this function allows adding funds to wallet
+    function addFunds(uint amount) public {
+        if(balance>= 10000) revert LimitReached();
+        if(msg.sender != gavin && !isAuthorized[msg.sender]) revert NotGavinOrAuthorized();
+        balance += amount;
+    }
+
+    //this function allows spending an amount to the account that has been granted access by Gavin
+    function spendFunds(uint amount) public {
+        if(msg.sender != gavin && !isAuthorized[msg.sender]) revert NotGavinOrAuthorized();
+        balance -= amount;
+    }
+
+    //this function grants access to an account and can only be accessed by Gavin
+    function addAccess(address x) public {
+        if(msg.sender != gavin) revert NotGavinOrAuthorized();
+        isAuthorized[x] = true;
+    }
+
+    //this function revokes access to an account and can only be accessed by Gavin
+    function revokeAccess(address x) public {
+        if(msg.sender != gavin) revert NotGavinOrAuthorized();
+        isAuthorized[x] = false;
+    }
+
+    //this function returns the current balance of the wallet
+    function viewBalance() public view returns(uint) {
+        if(msg.sender != gavin && !isAuthorized[msg.sender]) revert NotGavinOrAuthorized();
+        return(balance);
+    }
+
+}
